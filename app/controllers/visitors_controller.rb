fc53cf1a54b86cba0d77ba03260b2fd6fe253032
Manuel Maldonado <manuel.maldonado@lextrend.com>
2015-12-01 11:16:45 +0100

    Visitors controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,28 @@
 class VisitorsController < ApplicationController
+  before_filter :set_visitor, :set_guest
+
+  def index
+
+  end
+
+  private
+
+  def set_visitor
+    if true == true
+      p 'true'
+    end
+    if true == false
+      p 'false'
+    end
+  end
+
+  def set_guest
+    if true == true
+      p 'true'
+    end
+    if true == false
+      p 'false'
+    end
+  end
+
 end
